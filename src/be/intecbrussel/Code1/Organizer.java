1b1b0b8486cf9c38be6df6854383704f4b66180c
Apple <ellinemedrano1408@hotmail.com>
2021-01-18 12:48:49 +0100

    Updated

--- src/be/intecbrussel/Code1/Organizer.java
+++ src/be/intecbrussel/Code1/Organizer.java
@@ -1,10 +1,15 @@
 package be.intecbrussel.Code1;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Organizer {
     public static void main(String[] args) throws IOException {
@@ -12,9 +17,30 @@ public class Organizer {
         Files.walk(Paths.get("/Users/gast/Downloads/unsorted"))
         .forEach(path -> System.out.println(path.getFileName()));
 
+        System.out.println();
 
+        Path path = Paths.get("/Users/gast/Downloads/unsorted");
+        List<Path> paths = findByFileExtension(path, ".txt");
+        paths.forEach(x -> System.out.println(x.getFileName()));
     }
 
+    public static List<Path> findByFileExtension(Path path, String fileExtension)
+            throws IOException {
+
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
+        }
+
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk
+                    .filter(Files::isRegularFile)   // is a file
+                    .filter(p -> p.getFileName().toString().endsWith(fileExtension))
+                    .collect(Collectors.toList());
+        }
+        return result;
+
+    }
 
 
 }
