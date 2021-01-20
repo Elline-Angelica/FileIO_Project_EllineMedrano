9c3e3e9450cc1a3fef8c2d317c1b4a1736eb83cd
Apple <ellinemedrano1408@hotmail.com>
2021-01-20 19:55:38 +0100

    Updated

--- src/be/intecbrussel/service/Application.java
+++ src/be/intecbrussel/service/Application.java
@@ -11,17 +11,17 @@ public class Application {
         // organizer.manuallyMakeDirectories();
         // organizer.getFilesFromDirectory();
 
-//        FileOrganizer copyFiles = new FileOrganizer();
-//        copyFiles.copy("jpg", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/jpg");
-//        copyFiles.copy("csv", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/csv");
-//        copyFiles.copy("exe", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/exe");
-//        copyFiles.copy("gif", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/gif");
-//        copyFiles.copy("json", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/json");
-//        copyFiles.copy("pdf", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/pdf");
-//        copyFiles.copy("png", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/png");
-//        copyFiles.copy("py", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/py");
-//        copyFiles.copy("txt", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/text");
-//        copyFiles.copy("wma", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/wma");
+        FileOrganizer copyFiles = new FileOrganizer();
+        copyFiles.copy("jpg", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/jpg");
+        copyFiles.copy("csv", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/csv");
+        copyFiles.copy("exe", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/exe");
+        copyFiles.copy("gif", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/gif");
+        copyFiles.copy("json", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/json");
+        copyFiles.copy("pdf", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/pdf");
+        copyFiles.copy("png", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/png");
+        copyFiles.copy("py", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/py");
+        copyFiles.copy("txt", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/text");
+        copyFiles.copy("wma", "/Users/gast/Downloads/unsorted", "/Users/gast/Downloads/sorted/wma");
 
         try {
             FileWriter myWriter = new FileWriter("resources/summary.txt");
